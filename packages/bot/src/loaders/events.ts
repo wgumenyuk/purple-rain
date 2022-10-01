8772aa4e8545a158c78338f408a36ed4972c1ba1
wgumenyuk <wlad@gumenyuk.de>
2022-10-02 00:53:42 +0200

    Loader für Events hinzugefügt

---
+++ packages/bot/src/loaders/events.ts
@@ -0,0 +1,33 @@
+import path from "path";
+import glob from "glob-promise";
+
+// Types
+import type Aurelia from "@structs/aurelia";
+
+/**
+    Pfad zum Events-Ordner.
+*/
+const EVENTS_PATH = path.resolve(__dirname, "../events/*.js").replaceAll("\\", "/");
+
+/**
+    Lädt alle verfügbaren Events.
+*/
+const loadEvents = async (bot: Aurelia) => {
+    const start = performance.now();
+
+    const files = await glob(EVENTS_PATH);
+
+    for(const file of files) {
+        const handler = (await import(file)).default;
+        const event = path.basename(file).replace(".js", "");
+
+        bot.on(event, handler.bind(null, bot));
+    }
+
+    const end = performance.now();
+    const diff = Math.round(end - start);
+
+    bot.logger.info(`Events geladen (${diff}ms)`);
+};
+
+export default loadEvents;
