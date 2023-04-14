0da38eaa70e91c87edfa30b42227572ca1e9c0a0
wgumenyuk <wlad@gumenyuk.de>
2023-04-14 21:27:42 +0200

    ✨ feat: add index

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import path from "path";
+import { addAliases } from "module-alias";
+
+/**
+    Startet das Programm. 
+*/
+const start = async () => {
+    addAliases({
+        "$internal": path.join(__dirname, "./internal"),
+        "$loaders": path.join(__dirname, "./loaders"),
+        "$structs": path.join(__dirname, "./structs")
+    });
+
+    const Bot = (await import("$structs/bot")).default;
+    const bot = new Bot();
+
+    bot.start();
+};
+
+start();
