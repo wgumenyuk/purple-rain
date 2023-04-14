756de24f4885ae74bee41f930435ce8278cc16dc
wgumenyuk <wlad@gumenyuk.de>
2023-04-14 20:56:20 +0200

    ✨ feat: add event loader

---
+++ src/loaders/events.ts
@@ -0,0 +1,37 @@
+import path from "path";
+import glob from "fast-glob";
+
+// Types
+import type Bot from "$structs/bot";
+
+// #region Types
+/**
+    Event-Handler.
+*/
+type EventHandler = (...args: any[]) => Promise<void> | void;
+// #endregion
+
+/**
+    Glob-Pfad zum `events`-Ordner.
+*/
+const EVENTS_PATH = path
+    .join(__dirname, "../events/**/*.js")
+    .replaceAll("\\", "/");
+
+/**
+    Lädt alle Events aus dem `events`-Ordner.
+*/
+const loadEvents = async (bot: Bot) => {
+    const files = await glob(EVENTS_PATH);
+
+    for(const file of files) {
+        const eventName = path.basename(file).replace(".js", "");
+        const eventHandler: EventHandler = (await import(file)).default;
+
+        bot.on(eventName, eventHandler.bind(null, bot));
+    }
+
+    bot.logger.debug("Events geladen");
+};
+
+export default loadEvents;
