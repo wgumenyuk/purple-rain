cd4dd86d96d6f06865fbbdbfc2c143c7fc3d82e5
ar4ide <artjomgumenyuk08@gmail.com>
2023-04-17 21:22:00 +0200

    ✨ feat: add `version` command

---
+++ src/commands/system/version.ts
@@ -0,0 +1,37 @@
+import { EmbedBuilder, inlineCode } from "discord.js";
+
+// Intern
+import Command, { CommandGroup } from "$structs/command";
+import Color from "$internal/colors";
+
+// Types
+import type { Message } from "discord.js";
+import type Bot from "$structs/bot";
+
+/**
+    `version`-Befehl.
+*/
+class VersionCommand extends Command {
+    /**
+        Konstruktor.
+    */
+    constructor() {
+        super({
+            name: "version",
+            group: CommandGroup.SYSTEM,
+            description: "Zeigt die aktuelle Version des Bots an.",
+            examples: [ "version" ]
+        });
+    }
+
+    public async run(bot: Bot, message: Message<true>, _: any): Promise<void> {
+        const embed = new EmbedBuilder()
+            .setColor(Color.PURPLE)
+            .setTitle(`Version`)
+            .setDescription(`Purple Rain ${inlineCode(bot.version)}`);
+
+        message.channel.send({ embeds: [ embed ] });
+    }
+}
+
+export default VersionCommand;
