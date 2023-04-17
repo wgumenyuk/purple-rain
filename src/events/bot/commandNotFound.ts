faae6c0368c602d17b117b0effdf346546af23f3
ar4ide <artjomgumenyuk08@gmail.com>
2023-04-17 21:11:52 +0200

    📦 refactor: remove unused parameter

--- src/events/bot/commandNotFound.ts
+++ src/events/bot/commandNotFound.ts
@@ -5,13 +5,12 @@ import Color from "$internal/colors";
 
 // Types
 import type { Message } from "discord.js";
-import type Bot from "$structs/bot";
 
 /**
     Handler für `commandNotFound`.
     Wird ausgeführt, wenn ein Befehl nicht gefunden wird.
 */
-const onCommandNotFound = (bot: Bot, message: Message<true>, commandName: string) => {
+const onCommandNotFound = (_: any, message: Message<true>, commandName: string) => {
     const error = `Der Befehl ${inlineCode(commandName)} wurde nicht gefunden.`;
 
     const embed = new EmbedBuilder()
