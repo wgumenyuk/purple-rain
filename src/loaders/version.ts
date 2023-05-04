7cacf49da3a6aea507f7ed0c2be5b468d19d5fa0
wgumenyuk <wlad@gumenyuk.de>
2023-05-04 09:10:10 +0200

    🗑️ remove: `-dev` suffix when in dev mode

--- src/loaders/version.ts
+++ src/loaders/version.ts
@@ -7,20 +7,11 @@ import path from "path";
 const PACKAGE_JSON_PATH = path.join(__dirname, "../../package.json");
 
 /**
-    Node-Environment.
-*/
-const IS_DEV = (process.env.NODE_ENV !== "production");
-
-/**
     Lädt die Version aus `package.json`.
 */
 const loadVersion = () => {
     const file = fs.readFileSync(PACKAGE_JSON_PATH, "utf-8");
     let { version } = JSON.parse(file);
-
-    if(IS_DEV) {
-        version += "-dev";
-    }
 
     return `v${version}`;
 };
