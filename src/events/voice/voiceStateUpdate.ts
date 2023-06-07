5c54f55879b9d73239ae1e0c16029a53559d8fed
wgumenyuk <wlad@gumenyuk.de>
2023-06-08 00:33:35 +0200

    fix: faulty `voiceStateUpdate` handler

--- src/events/voice/voiceStateUpdate.ts
+++ src/events/voice/voiceStateUpdate.ts
@@ -1,7 +1,7 @@
 import { Events } from "discord.js";
 
 // Types
-import type { VoiceState } from "discord.js";
+import type { VoiceBasedChannel, VoiceState } from "discord.js";
 import type { Bot } from "$structs/bot";
 
 /**
@@ -10,9 +10,22 @@ import type { Bot } from "$structs/bot";
 export const name = Events.VoiceStateUpdate;
 
 /**
+    Überprüft, ob der Bot sich alleine in einem Voice-Channel
+    befindet.
+*/
+const _isBotAlone = (botId: string, channel: VoiceBasedChannel | null) => {
+    return (
+        !!channel &&
+        channel.members.has(botId) &&
+        channel.members.size === 1
+    );
+};
+
+/**
     Wird ausgeführt, wenn ein Voice-Zustand sich aktualisiert.
 */
 export const handler = (bot: Bot, oldState: VoiceState, newState: VoiceState) => {
+    const botId = bot.user!.id;
     const guildId = oldState.guild.id;
 
     if(!bot.vinyl.hasQueue(guildId)) {
@@ -22,17 +35,21 @@ export const handler = (bot: Bot, oldState: VoiceState, newState: VoiceState) =>
     const oldChannel = oldState.channel;
     const newChannel = newState.channel;
 
-    const hasBotMoved =
-        !!oldChannel &&
-        !!newChannel &&
-        oldChannel.id !== newChannel.id &&
-        newChannel.members.has(bot.user!.id);
+    const isBotInChannel =
+        oldChannel?.members.has(botId) ||
+        newChannel?.members.has(botId);
 
-    if(!hasBotMoved) {
+    if(!isBotInChannel) {
         return;
     }
 
-    const queue = bot.vinyl.getQueue(guildId);
+    const isBotAlone =
+        _isBotAlone(botId, oldChannel) ||
+        _isBotAlone(botId, newChannel);
 
-    queue.disconnect();
+    if(!isBotAlone) {
+        return;
+    }
+
+    bot.vinyl.removeQueue(guildId);
 };
