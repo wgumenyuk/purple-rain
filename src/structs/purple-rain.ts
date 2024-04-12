c8d60cb735507b953f11420bf258b3e32d341e23
wgumenyuk <wgumenyuk.gg@gmail.com>
2024-04-12 09:41:32 +0200

    feat: run config loader in `init()`

--- src/structs/purple-rain.ts
+++ src/structs/purple-rain.ts
@@ -2,9 +2,11 @@ import { Client, GatewayIntentBits as Intents } from "discord.js";
 
 // Intern
 import { createLogger } from "$structs/logger";
+import { loadConfig } from "$loaders/config";
 
 // Types
 import type { Logger } from "pino";
+import type { Config } from "$loaders/config";
 
 /**
   Intents für Purple Rain.
@@ -22,6 +24,12 @@ const INTENTS: Intents[] = [
 */
 export class PurpleRain extends Client {
   /**
+    Konfiguration.
+  */
+  // @ts-expect-error: `config` wird mit `init()` geladen.
+  public config: Config = null;
+
+  /**
     Logger.
   */
   public log: Logger;
@@ -37,5 +45,24 @@ export class PurpleRain extends Client {
     this.log = createLogger({
       msgPrefix: "[bot] "
     });
+  }
+
+  /**
+    Initialisiert Purple Rain.
+  */
+  public async init() {
+    this.log.info("initializing");
+
+    // Loader ausführen.
+    await Promise.allSettled([
+      loadConfig(this)
+    ]);
+
+    try {
+      await this.login(this.config.token);
+    } catch(err) {
+      this.log.fatal(err, "failed to initialize");
+      process.exit(1);
+    }
   }
 };
