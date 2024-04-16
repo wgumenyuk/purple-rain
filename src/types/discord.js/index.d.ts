68d54aff262044dee8013e492b18cc3ce5b1a6fa
Wlad G. Gumenyuk <wlad@gumenyuk.de>
2024-04-16 20:57:48 +0200

    feat: add `commandError` event

--- src/types/discord.js/index.d.ts
+++ src/types/discord.js/index.d.ts
@@ -1,6 +1,13 @@
+import type { Command } from "$structs/command";
+
 declare module "discord.js" {
   // Events erweitern.
   export interface ClientEvents {
+    commandError: [
+      message: Message<true>,
+      command: Command,
+      err: Error
+    ];
     commandNotFound: [
       message: Message<true>,
       commandName: string
