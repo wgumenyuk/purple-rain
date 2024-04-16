3caa89217160dc6e89b8b593a195dcf53317a5c6
Wlad G. Gumenyuk <wlad@gumenyuk.de>
2024-04-16 21:18:36 +0200

    feat: add Levenshtein distance for `commandNotFound` event handler

--- src/events/command-not-found.ts
+++ src/events/command-not-found.ts
@@ -1,4 +1,8 @@
 import { Colors, EmbedBuilder, inlineCode } from "discord.js";
+import {
+  closest as getClosest,
+  distance as getDistance
+} from "fastest-levenshtein";
 
 // Types
 import type { EventHandler } from "$structs/event-handler";
@@ -19,6 +23,20 @@ export const handle: EventHandler<"commandNotFound"> = function(
   const embed = new EmbedBuilder()
     .setColor(Colors.Red)
     .setDescription(`Der Befehl ${inlineCode(commandName)} wurde nicht gefunden.`);
+
+  const commands = bot.commands.map((command) => command.name);
+
+  // Levenshtein-Distanz ermitteln.
+  const closestCommandName = getClosest(commandName, commands);
+  const distance = getDistance(commandName, closestCommandName);
+
+  // Ähnlichsten Befehl hinzufügen, wenn Levenshtein-Distanz <= 2.
+  if(distance <= 2) {
+    embed.addFields({
+      name: "Ähnlichster Befehl",
+      value: inlineCode(closestCommandName)
+    });
+  }
 
   message.channel.send({
     embeds: [
